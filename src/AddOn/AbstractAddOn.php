224e0244fe4893f48c3fe310b903295dfc94f14a
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2024-06-16 18:32:00 +0200

    Add type to addon contract

--- src/AddOn/AbstractAddOn.php
+++ src/AddOn/AbstractAddOn.php
@@ -7,6 +7,13 @@ namespace Mammatus\Kubernetes\Contracts\AddOn;
 interface AbstractAddOn extends \JsonSerializable
 {
     /**
+     * The type of addon:
+     * container
+     * deployment
+     */
+    public function type(): string;
+
+    /**
      * The Helper to call
      */
     public function helper(): string;
