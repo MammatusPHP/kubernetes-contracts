f51e37651dda3ea5fa04664383f119cbfaa0ca9a
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-12-27 19:23:32 +0100

    Add Pod, CronJob, and StatefulSet marker interfaces

---
+++ src/AddOn/CronJob.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mammatus\Kubernetes\Contracts\AddOn;
+
+interface CronJob extends AbstractAddOn
+{
+}
---
+++ src/AddOn/StatefulSet.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mammatus\Kubernetes\Contracts\AddOn;
+
+interface StatefulSet extends AbstractAddOn
+{
+}
